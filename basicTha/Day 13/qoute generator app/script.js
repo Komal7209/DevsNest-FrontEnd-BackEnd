ba0dc186b1fbda2b43e4678f392d1bb3fd9bd640
Komal Pal <33154959+Komal7209@users.noreply.github.com>
2021-08-01 07:08:08 +0000

    contest api

--- basicTha/Day 13/qoute generator app/script.js
+++ basicTha/Day 13/qoute generator app/script.js
@@ -1,67 +1,23 @@
-fetch("https://codeforces.com/api/contest.list?gym=false")
-  .then((res) => res.json())
-  .then((data) => {
-    data.result.forEach((contestObj) => {
-      if (contestObj.phase == "BEFORE") {
-        addContestTohtml(contestObj);
-      }
-    });
-  });
+const quoteDiv = document.getElementById("quoteDiv");
+const btn = document.getElementById("btn");
 
-let container = document.querySelector(".container");
+btn.addEventListener("click", generateQuote);
 
-function addContestTohtml(contestObj) {
-  let position = "beforeend";
+generateQuote();
 
-  let startTimeMillisecs = contestObj.startTimeSeconds * 1000;
-  let startTime = unixMillisecsToHumanReadable(startTimeMillisecs);
-  let day = startTime.toLocaleString("en-US", { weekday: "long" });
-  let date = startTime.toLocaleString("en-US", {
-    month: "short",
-    day: "numeric",
-    year: "numeric",
-  });
+async function generateQuote() {
+  const res = await fetch("https://api.quotable.io/random");
+  const data = await res.json();
 
-  let currDateTimeUNIX = new Date();
-  const oneDay = 1000 * 60 * 60 * 24;
-
-  // Calculating the time difference between two dates
-  let diffInTime = startTimeMillisecs - currDateTimeUNIX.getTime();
-
-  // Calculating the no. of days between two dates
-  let diffInDays;
-  if (diffInTime >= oneDay) {
-    diffInDays = Math.round(diffInTime / oneDay) + " Days";
-  } else {
-    diffInTime = diffInTime / 1000;
-    let hours = Math.floor(diffInTime / 3600);
-    diffInTime = diffInTime % 3600;
-    let minutes = Math.floor(diffInTime / 60);
-    let secs = Math.floor(diffInTime % 60);
-    diffInDays = `${hours} : ${minutes} : ${secs}`;
-  }
-
-  // console.log(contestObj.durationSeconds);
-
-  // let duration = contestObj.durationSeconds;
-  // let durationHours = Math.floor(duration / 3600);
-  // duration = duration - durationHours;
-  // let durationMins = Math.floor(duration / 60);
-
-  // duration = `${durationHours} : ${durationMins}`;
-
-  let addhtml = ` <div class="contest-card">
-                    <div class="contest-name"><a href="https://codeforces.com/contests">${contestObj.name}</a></div>
-                    <div class="day">Day: ${day}</div>
-                    <div class="start-time">Date: ${date}</div>
-                    <div class="time-to-start">Starts In: ${diffInDays}</div>
-                   
-                  </div>`;
-
-  container.insertAdjacentHTML(position, addhtml);
+  quoteDiv.innerHTML = data.content;
 }
 
-function unixMillisecsToHumanReadable(unixMilli) {
-  let dateObj = new Date(unixMilli);
-  return dateObj;
-}
+// Using fetchAPI
+
+// function generateQuote() {
+//   fetch("https://api.quotable.io/random")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     quoteDiv.innerHTML = data.content;
+//   })
+// }
