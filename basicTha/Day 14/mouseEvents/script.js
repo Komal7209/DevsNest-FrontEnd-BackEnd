8f9dca366119b6cc93848b59aeab140a8c321177
Komal Pal <33154959+Komal7209@users.noreply.github.com>
2021-08-23 15:20:51 +0000

    drag events tha completed

--- basicTha/Day 14/mouseEvents/script.js
+++ basicTha/Day 14/mouseEvents/script.js
@@ -2,6 +2,8 @@
 const container = document.querySelector(".container");
 const box = document.querySelector(".box");
 
+//The querySelector() method only returns the first element that matches the specified selectors. To return all the matches, use the querySelectorAll() method instead.
+
 container.addEventListener("click", (e) => {
   const x = e.clientX;
   const y = e.clientY;
